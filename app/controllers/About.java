ac2cc41c656759ef39e1fbc02d494e96f3e87194
Farsheed <farshsomo@gmail.com>
2014-07-12 21:49:29 +0200

    About Page created, not complete

--- app/controllers/About.java
+++ app/controllers/About.java
@@ -12,14 +12,29 @@ import play.mvc.Http.Context;
 
 public class About extends Controller{
 	
-	public static Result display(){
+	public static Result display(String lang){
 		
-		if(session().containsKey("email"))
-			return ok(about.render(User.find.byId(session().get("email"))));
-		else{
-			User guest = new User("Guest","dummyEmail","dummyPassword");
-			return ok(about.render(guest));
-			
+		if(lang.equals("english")){
+			if(session().containsKey("email"))
+				return ok(about.render(User.find.byId(session().get("email"))));
+			else{
+				User guest = new User("Guest","dummyEmail","dummyPassword");
+				return ok(about.render(guest));
+			}	
+		}else if(lang.equals("farsi")){
+			if(session().containsKey("email"))
+				return ok(views.html.farsiEdition.about.render(User.find.byId(session().get("email"))));
+			else{
+				User guest = new User("Guest","dummyEmail","dummyPassword");
+				return ok(views.html.farsiEdition.about.render(guest));
+			}
 		}
+		else{
+			//if neither english nor farsi is selected
+			return badRequest("ERROR : The entered Language is not supported! PLease choose either Farsi or English");
+		}
+	
+	
+	
 	}
 }
