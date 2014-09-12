2f195f3766e31206cdfdded7134eca5972477a2a
Farsheed <farshsomo@gmail.com>
2014-09-12 04:12:09 +0200

    comment text layout improved

--- app/controllers/Books.java
+++ app/controllers/Books.java
@@ -7,15 +7,30 @@ import views.html.*;
 
 public class Books extends Controller{
 	
-	public static Result display(){
+	public static Result display(String lang){
 		
-		if(session().containsKey("email"))
-			return ok(books.render(User.find.byId(session().get("email"))));
+		
+		if(lang.equals("english")){
+			if(session().containsKey("email"))
+				return ok(books.render(User.find.byId(session().get("email"))));
+			else{
+				User guest = new User("Guest","dummyEmail","dummyPassword");
+				return ok(books.render(guest));
+			}	
+		}else if(lang.equals("farsi")){
+			if(session().containsKey("email"))
+				return ok(views.html.farsiEdition.books.render(User.find.byId(session().get("email"))));
+			else{
+				User guest = new User("Guest","dummyEmail","dummyPassword");
+				return ok(views.html.farsiEdition.books.render(guest));
+			}
+		}
 		else{
-			User guest = new User("Guest","dummyEmail","dummyPassword");
-			return ok(books.render(guest));
-			
+			//if neither english nor farsi is selected
+			return badRequest("ERROR : The entered Language is not supported! PLease choose either Farsi or English");
 		}
 	}
+	
+
 
 }
--- app/controllers/Services.java
+++ app/controllers/Services.java
@@ -1,21 +1,37 @@
 package controllers;
 
+import controllers.About.FeedBack;
 import models.User;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
 
 public class Services extends Controller{
 	
-	public static Result display(){
+	public static Result display(String lang){
 		
-		if(session().containsKey("email"))
-			return ok(services.render(User.find.byId(session().get("email"))));
+		if(lang.equals("english")){
+			if(session().containsKey("email"))
+				return ok(services.render(User.find.byId(session().get("email"))));
+			else{
+				User guest = new User("Guest","dummyEmail","dummyPassword");
+				return ok(services.render(guest));
+			}	
+		}else if(lang.equals("farsi")){
+			if(session().containsKey("email"))
+				return ok(views.html.farsiEdition.services.render(User.find.byId(session().get("email"))));
+			else{
+				User guest = new User("Guest","dummyEmail","dummyPassword");
+				return ok(views.html.farsiEdition.services.render(guest));
+			}
+		}
 		else{
-			User guest = new User("Guest","dummyEmail","dummyPassword");
-			return ok(services.render(guest));
-			
+			//if neither english nor farsi is selected
+			return badRequest("ERROR : The entered Language is not supported! PLease choose either Farsi or English");
 		}
 	}
 
+	
+	
 }
