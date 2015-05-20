5d591ad16916df74b80a839431478a8d76e4b232
FarsheedM <farshsomo@gmail.com>
2015-05-20 18:00:56 +0200

    Therapy page content in place

--- app/controllers/Therapy.java
+++ app/controllers/Therapy.java
@@ -1,7 +1,13 @@
 package controllers;
 
+import org.joda.time.DateTime;
+
+import com.typesafe.plugin.MailerAPI;
+import com.typesafe.plugin.MailerPlugin;
+
 import controllers.About.FeedBack;
 import play.data.Form;
+import play.data.validation.Constraints.Required;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.*;
@@ -13,17 +19,17 @@ public class Therapy extends Controller{
 		
 		if(lang.equals("english")){
 			if(session().containsKey("email"))
-				return ok(therapy.render(User.find.byId(session().get("email"))));
+				return ok(therapy.render(User.find.byId(session().get("email")),Form.form(Feedback.class)));
 			else{
 				User guest = new User("Guest","dummyEmail","dummyPassword");
-				return ok(therapy.render(guest));
+				return ok(therapy.render(guest,Form.form(Feedback.class)));
 			}	
 		}else if(lang.equals("farsi")){
 			if(session().containsKey("email"))
-				return ok(views.html.farsiEdition.therapy.render(User.find.byId(session().get("email"))));
+				return ok(views.html.farsiEdition.therapy.render(User.find.byId(session().get("email")),Form.form(Feedback.class)));
 			else{
 				User guest = new User("Guest","dummyEmail","dummyPassword");
-				return ok(views.html.farsiEdition.therapy.render(guest));
+				return ok(views.html.farsiEdition.therapy.render(guest,Form.form(Feedback.class)));
 			}
 		}
 		else{
@@ -32,4 +38,54 @@ public class Therapy extends Controller{
 		}
 	}
 
+	
+	//this method is called when the user post the new feedback(in therapy view)
+	//It sends the feedback to the Admin
+	public static Result postFeedback(String language){
+		
+		User usr;
+		User guest = new User("Guest","dummyEmail","dummyPassword");
+		if(session().containsKey("email"))
+			usr = User.find.byId(session().get("email"));
+		else
+			usr = guest;
+		
+		try{
+			Form<Feedback> feedBackForm = Form.form(Feedback.class).bindFromRequest();
+			
+	    	if(feedBackForm.hasErrors()){
+	    		if(language.equals("farsi"))
+	    			return badRequest(views.html.farsiEdition.alert.render(usr,"متاسفانه پیام شما ارسال نشد! لطفا دوباره پیام خود را بفرستید."));
+	    		else
+	    			return badRequest(views.html.alert.render(usr,"Your message has not been sent! Please Try again!"));
+			} else {
+				
+			    MailerAPI mail = play.Play.application().plugin(MailerPlugin.class).email();
+			    mail.setSubject(feedBackForm.get().subject);
+			    mail.addRecipient("farsireads@gmail.com");
+			    mail.addFrom(usr.email);
+			    mail.send("A text only message", "<html><body><p>sent by <b>"+usr.lName+"-"+usr.fName+
+			    		"</b><br>"+feedBackForm.get().feedback+"</p></body></html>" );
+			}
+		}
+		catch(Exception ex){
+			System.err.println(ex.getMessage());
+		}
+		if(language.equals("farsi"))
+			return ok(views.html.farsiEdition.alert.render(usr,"سپاس، ارزیابی شما با موفقیت فرستاده شد. پیام شما پس از بررسی‌، برای استفاده دیگران منتشر میشود.."));
+		else
+			return ok(views.html.alert.render(usr,"Thanks!  Your evaluation sent successfully. We will reply soon."));
+
+	}
+
+	public static class Feedback{
+		
+		public String name;
+		public String email;
+		@Required
+		public String subject;
+		@Required
+		public String feedback;
+		
+	}
 }
